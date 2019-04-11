d5ac21e53507287fda5af98cf59d1a1d62a058ba
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-10 20:05:29 -0500

    Update UserInputSentiment.py

--- UserInputSentiment.py
+++ UserInputSentiment.py
@@ -1,20 +1,32 @@
-# more simplistic than gathering text from an API
-# in this version one provides the text to test the sentiment analysis
+# This version is a more simplistic implementation
+# As opposed to gathering text from an API or web scraping.
+# In this version one provides the text in run-time to process for sentiment analysis
 
 # Importing TextBlob package
-
 from textblob import TextBlob
 
-print("\nHere is the most basic of Sentiment Analysis.\n")
-text1 = 'Python is boring'
-text2 = 'Python is the most superior programming language!'
+print("------------------------------",
+      "\n| Sentiment Analysis Machine |",
+      "\n|        'q' to quit         |",
+      "\n------------------------------\n")
 
-# attempting to pass the texts to a dataframe to be able to print sentiments of multiple text blocks
-obj = TextBlob(text1)
-sentiment = obj.sentiment.polarity
-print(text1, ", Sentiment: ", sentiment)
 
-obj = TextBlob(text2)
-sentiment = obj.sentiment.polarity
-print(text2, ", Sentiment: ", sentiment)
-# simple, inputted text demonstration
+def sentiment_analysis():
+    # Ask user for input
+    userInput = input("Enter text to analyze: \n")
+
+    if userInput != "q":
+
+        obj = TextBlob(userInput)  # set object of user input
+        sentiment = obj.sentiment.polarity  # pass object to text-blob library
+        print("sentiment: ", sentiment, "\n")  # prints polarity
+
+        sentiment_analysis()  # loops function again
+
+
+sentiment_analysis()
+print("\nThank you for using my machine")
+
+# The outputted polarity ranges from negative (-1)
+# to positive (1), more neutral comments tend to give out
+# a polarity between -0.2 <-> 0.2
